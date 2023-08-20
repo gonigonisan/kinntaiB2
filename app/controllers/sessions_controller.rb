2c3aaf9d899b78c4836ea45a60b1fbc88d19cfb0
HITOMI FURUKAWA <pankun0321@gmail.com>
2023-08-20 04:35:32 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # &&は取得したユーザーオブジェクトが有効か判定するため
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = "証人に失敗しました。"
